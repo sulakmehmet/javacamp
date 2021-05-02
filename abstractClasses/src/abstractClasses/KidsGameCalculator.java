8f88906767f342c6f423e1f6939de8d4ba711e2a
sulakmehmet <sulakmhmt@gmail.com>
2021-05-02 22:43:57 +0300

    Fourth day homework

---
+++ abstractClasses/src/abstractClasses/KidsGameCalculator.java
@@ -0,0 +1,12 @@
+package abstractClasses;
+
+public class KidsGameCalculator extends GameCalculator {
+
+	@Override
+	public void hesapla() {
+		System.out.println("Puanýnýz : 100");
+		
+	}
+	
+
+}
